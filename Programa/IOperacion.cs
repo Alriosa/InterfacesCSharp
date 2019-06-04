15bad8513b5bbd1e2130dcc95eeaca07fb046c47
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-06-04 10:07:43 -0600

    2

---
+++ Programa/IOperacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa
+{
+    interface IOperacion
+    {
+        void calcular(double numero1, double numero2);
+        void mostrar();
+    }
+}
---
+++ Programa/Suma.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa
+{
+    class Suma : IOperacion //Aqui estoy implementando herencia
+    {
+        private double resultado = 0;
+
+        private ArrayList resultados = new ArrayList();
+
+        //Metodos que se deben implementar OBLIGATORIAMENTE
+        public void calcular(double numero1, double numero2)
+        {
+            resultado = numero1 + numero2;
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("El resultado de la suma es {0}" , resultados);
+            resultados.Add(resultado);
+        }
+
+        public void MuestraResultados()
+        {
+            foreach(double resultado in resultados)
+                Console.WriteLine(resultado);
+        }
+    }
+}
